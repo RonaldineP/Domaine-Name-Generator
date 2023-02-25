e449f72f7b52f2324f14706a0863ca17f43f4a49
RonaldineP <ronaldinephanor@gmail.com>
2023-02-25 03:23:20 +0000

    domaine-generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
+//import "bootstrap";
+///:import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//window.onload = function() {
+//write your code here
+//console.log("Hello Rigo from the console!");
+//};
+
+//let pronoun = ["the", "our", "that"];
+//let adj = ["great", "big", "small"];
+//let noun = ["jogger", "racom", "feeorg"];
+//let extensions = [".com", ".net", ".org"];
+//for (let item of pronoun) {
+//for (let word of adj) {
+//for (let object of noun) {
+//for (let variation of extensions) {
+//console.log(`${item}${word}${object}${variation}`);
+// }
+//}
+//}
+//}
+
+let pronoun = ["the", "our", "that"];
+let adj = ["great", "big", "small"];
+let noun = ["joggernet", "racomcom", "feeorg"];
+let extensions = [".com", ".net", ".org"];
+
+for (let item of pronoun) {
+  for (let word of adj) {
+    for (let object of noun) {
+      let randomExtension =
+        extensions[Math.floor(Math.random() * extensions.length)];
+      console.log(`${item}${word}${object.slice(0, -3)}${randomExtension}`);
+    }
+  }
+}

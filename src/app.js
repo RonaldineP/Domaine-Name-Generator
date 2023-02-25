0d9fd2863f55e5af0ac6466f3b74aa183062c999
RonaldineP <ronaldinephanor@gmail.com>
2023-02-25 03:50:19 +0000

    Domaine Name Generator

--- src/app.js
+++ src/app.js
@@ -1,15 +1,3 @@
-/* eslint-disable */
-//import "bootstrap";
-///:import "./style.css";
-
-//import "./assets/img/rigo-baby.jpg";
-//import "./assets/img/4geeks.ico";
-
-//window.onload = function() {
-//write your code here
-//console.log("Hello Rigo from the console!");
-//};
-
 //let pronoun = ["the", "our", "that"];
 //let adj = ["great", "big", "small"];
 //let noun = ["jogger", "racom", "feeorg"];
@@ -24,6 +12,25 @@
 //}
 //}
 
+//BONUS 1
+
+//let pronoun = ["the", "our", "that"];
+//let adj = ["great", "big", "small"];
+//let noun = ["joggernet", "racomcom", "feeorg"];
+//let extensions = [".com", ".net", ".org"];
+
+//for (let item of pronoun) {
+//for (let word of adj) {
+//for (let object of noun) {
+//let randomExtension =
+//extensions[Math.floor(Math.random() * extensions.length)];
+//console.log(`${item}${word}${object.slice(0, -3)}${randomExtension}`);
+//}
+//}
+//}
+
+//BONUS 2
+
 let pronoun = ["the", "our", "that"];
 let adj = ["great", "big", "small"];
 let noun = ["joggernet", "racomcom", "feeorg"];
@@ -34,7 +41,8 @@ for (let item of pronoun) {
     for (let object of noun) {
       let randomExtension =
         extensions[Math.floor(Math.random() * extensions.length)];
-      console.log(`${item}${word}${object.slice(0, -3)}${randomExtension}`);
+      let modifiedObject = object.replace(/.{3}$/, randomExtension); // replace last 3 characters with randomExtension
+      console.log(`${item}${word}${modifiedObject}`);
     }
   }
 }
